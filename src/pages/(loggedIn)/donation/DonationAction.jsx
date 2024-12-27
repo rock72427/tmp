5323a03a4271aaa1a4c50cee8444d21113dda95b
rock72427 <165383754+rock72427@users.noreply.github.com>
2024-12-27 16:03:09 +0530

    commit

--- src/pages/(loggedIn)/donation/DonationAction.jsx
+++ src/pages/(loggedIn)/donation/DonationAction.jsx
@@ -1,10 +1,7 @@
 import "./DonationAction.scss";
 import useDonationStore from "../../../../donationStore";
 import { useAuthStore } from "../../../../store/authStore";
-import {
-  createNewReceiptDetail,
-  fetchUniqueNumbers,
-} from "../../../../services/src/services/receiptDetailsService";
+import { createNewReceiptDetail } from "../../../../services/src/services/receiptDetailsService";
 import { createNewGuestDetails } from "../../../../services/src/services/guestDetailsService";
 import { createNewDonation } from "../../../../services/src/services/donationsService";
 import { useEffect, useState } from "react";
@@ -21,22 +18,6 @@ const DonationAction = ({ totalAmount = 0, activeTab, transactionType }) => {
   useEffect(() => {
     console.log("user", user);
   }, [user]);
-
-  useEffect(() => {
-    const getUniqueNumbers = async () => {
-      try {
-        const response = await fetchUniqueNumbers();
-        response.data.map((item) => {
-          console.log(item.attributes.unique_no);
-        });
-        setUniqueNumbers(response);
-      } catch (error) {
-        console.error("Error fetching unique numbers:", error);
-      }
-    };
-
-    getUniqueNumbers();
-  }, []);
 
   const validateFields = () => {
     const currentTab = donorTabs[activeTabId][currentSection];
