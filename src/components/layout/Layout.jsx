7f2d86c136280581273b6c5c2db8a3a32a0aff0e
rock72427 <165383754+rock72427@users.noreply.github.com>
2024-12-28 11:35:53 +0530

    commit

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -23,31 +23,34 @@
 
 // export default Layout;
 
-
-import React from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
-import Header from '../ui/header/Header';
-import Sidebar from '../ui/sidebar/Sidebar';
-import './Layout.scss';
+import React from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import Header from "../ui/header/Header";
+import Sidebar from "../ui/sidebar/Sidebar";
+import "./Layout.scss";
 
 const Layout = () => {
-    const location = useLocation();
-    const { pathname } = location;
+  const location = useLocation();
+  const { pathname } = location;
 
-    // Define the paths where elements should be hidden
-    const hideElements = ['/donation', '/donationdetail', '/deeksha'].includes(pathname);
+  // Define the paths where elements should be hidden
+  const hideElements = ["/donation", "/donationdetail", "/deeksha"].includes(
+    pathname
+  );
 
-    return (
-        <div className="layout-container">
-            <Sidebar />
-            <div className={`content ${pathname === '/dashboard' ? 'dashboard' : ''}`}>
-                <Header hideElements={hideElements} />
-                <div className="outlet">
-                    <Outlet />
-                </div>
-            </div>
+  return (
+    <div className="layout-container">
+      <Sidebar />
+      <div
+        className={`content ${pathname === "/dashboard" ? "dashboard" : ""}`}
+      >
+        <Header hideElements={hideElements} />
+        <div className="outlet">
+          <Outlet />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Layout;
--- src/components/ui/sidebar/Sidebar.jsx
+++ src/components/ui/sidebar/Sidebar.jsx
@@ -4,7 +4,7 @@ import "./Sidebar.scss";
 import { icons } from "../../../constants";
 import { useAuthStore } from "../../../../store/authStore";
 
-const Sidebar = () => {
+const Sidebar = ({ isOpen, toggleSidebar }) => {
   const location = useLocation();
   const navigate = useNavigate();
   const isDashboard = location.pathname === "/dashboard";
@@ -17,30 +17,46 @@ const Sidebar = () => {
   };
 
   return (
-    <div className={`sidebar ${isDashboard ? "dashboard" : ""}`}>
-      <NavLink to="/dashboard" activeclassname="active">
-        <img src={icons.Home} alt="home" />
-        <span className="label">Guest House</span>
-      </NavLink>
-      <NavLink to="/newDonation" activeclassname="active">
-        <img src={icons.Donation} alt="donation" />
-        <span className="label" style={{ paddingTop: "5px" }}>
-          Donations
-        </span>
-      </NavLink>
-      <NavLink to="/deeksha" activeclassname="active">
-        <img src={icons.Deeksha} alt="welcome" />
-        <span className="label">Deeksha</span>
-      </NavLink>
-      <NavLink to="/coupons" activeclassname="active">
-        <img src={icons.Frame} alt="welcome" />
-        <span className="label">Meals</span>
-      </NavLink>
-      <NavLink className="settings" onClick={handleLogout}>
-        <img src={icons.settings} alt="settings" />
-        <span className="label">Settings</span>
-      </NavLink>
-    </div>
+    <>
+      <div className="hamburger" onClick={toggleSidebar}>
+        {isOpen ? (
+          <i className="fas fa-times"></i>
+        ) : (
+          <i className="fas fa-bars"></i>
+        )}
+      </div>
+      <div
+        className={`sidebar ${isDashboard ? "dashboard" : ""} ${
+          isOpen ? "open" : ""
+        }`}
+      >
+        <div className="close-btn" onClick={toggleSidebar}>
+          <i className="fas fa-times"></i>
+        </div>
+        <NavLink to="/dashboard" activeclassname="active">
+          <img src={icons.Home} alt="home" />
+          <span className="label">Guest House</span>
+        </NavLink>
+        <NavLink to="/newDonation" activeclassname="active">
+          <img src={icons.Donation} alt="donation" />
+          <span className="label" style={{ paddingTop: "5px" }}>
+            Donations
+          </span>
+        </NavLink>
+        <NavLink to="/deeksha" activeclassname="active">
+          <img src={icons.Deeksha} alt="welcome" />
+          <span className="label">Deeksha</span>
+        </NavLink>
+        <NavLink to="/coupons" activeclassname="active">
+          <img src={icons.Frame} alt="welcome" />
+          <span className="label">Meals</span>
+        </NavLink>
+        <NavLink className="settings" onClick={handleLogout}>
+          <img src={icons.settings} alt="settings" />
+          <span className="label">Settings</span>
+        </NavLink>
+      </div>
+    </>
   );
 };
 
